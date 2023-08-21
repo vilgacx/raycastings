3f1d97253bdc84ac8941194f078d8ef48dbe4fbe
xorvet <cypherx702@gmail.com>
2023-08-21 23:24:20 +0530

    update

--- py/main.py
+++ py/main.py
@@ -12,31 +12,45 @@ MAP = [
 ]
 
 pygame.init()
-
-pygame.display.set_caption("raycastingdotpy")
+pygame.display.set_caption("title")
 screen = pygame.display.set_mode((1280, 720))
+running = True
 
 L = 50
 
-y = 0
-for i in MAP:
-    x = 0
-    for j in i:
-        if j == 1:
-            pygame.draw.rect(screen, "white", pygame.Rect(x,y,L,L),1)
-        x += L
-    del x
-    y += L
-del y
+player_x = 60
+player_y = 60
 
-running = True
+
+def drawMap():
+    y = 0
+    for i in MAP:
+        x = 0
+        for j in i:
+            if j == 1:
+                pygame.draw.rect(screen, "white", pygame.Rect(x,y,L,L),1)
+            x += L
+        del x
+        y += L
+    del y
 
 while running:
+    screen.fill("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
+            running = False 
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_DOWN]:
+            player_y += 5
+        elif keys[pygame.K_UP]:
+            player_y -= 5
+        elif keys[pygame.K_RIGHT]:
+            player_x += 5
+        elif keys[pygame.K_LEFT]:
+            player_x -= 5
+    drawMap()
+    pygame.draw.circle(screen, "green", (player_x,player_y),10)
     pygame.display.flip()
-
 
 pygame.quit()
