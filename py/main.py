a337f9266f779b4df204e5806977bade541c5988
xorvet <cypherx702@gmail.com>
2023-08-23 00:32:17 +0530

    minor update

--- py/main.py
+++ py/main.py
@@ -11,24 +11,29 @@ MAP = [
     [1,1,1,1,1,1,1,1],
 ]
 
+L = 40
+STEPS = 2
+
 pygame.init()
 pygame.display.set_caption("title")
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
-L = 50
+player_x = (L * 1) + 20
+player_y = (L * 1) + 20
 
-player_x = 60
-player_y = 60
-
+Rects = []
 
 def drawMap():
+    Rects.clear()
     y = 0
+    
     for i in MAP:
         x = 0
-        for j in i:
+        for j in i: 
             if j == 1:
-                pygame.draw.rect(screen, "white", pygame.Rect(x,y,L,L),1)
+                rect = pygame.draw.rect(screen, "white", pygame.Rect(x,y,L,L),1)
+                Rects.append(rect)
             x += L
         del x
         y += L
@@ -42,15 +47,18 @@ while running:
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_DOWN]:
-            player_y += 5
+            player_y += STEPS
         elif keys[pygame.K_UP]:
-            player_y -= 5
+            player_y -= STEPS
         elif keys[pygame.K_RIGHT]:
-            player_x += 5
+            player_x += STEPS
         elif keys[pygame.K_LEFT]:
-            player_x -= 5
+            player_x -= STEPS
+
     drawMap()
-    pygame.draw.circle(screen, "green", (player_x,player_y),10)
+    player = pygame.draw.circle(screen, "green", (player_x,player_y),10)
+    #player.collidelistall(Rects)
+    
     pygame.display.flip()
 
 pygame.quit()
