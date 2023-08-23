06a240ed8563b01011f7049e0c2a5eac95661892
xorvet <cypherx702@gmail.com>
2023-08-23 21:16:21 +0530

    fing collision detection

--- py/main.py
+++ py/main.py
@@ -1,4 +1,5 @@
 import pygame
+from operator import itemgetter
 
 MAP = [
     [1,1,1,1,1,1,1,1],
@@ -12,25 +13,23 @@ MAP = [
 ]
 
 L = 40
-STEPS = 2
+STEPS = 4
 
 pygame.init()
 pygame.display.set_caption("title")
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
-player_x = (L * 1) + 20
-player_y = (L * 1) + 20
-
+player_x = 60
+player_y = 60
 Rects = []
 
 def drawMap():
     Rects.clear()
-    y = 0
-    
+    y = 0 
     for i in MAP:
         x = 0
-        for j in i: 
+        for j in i:
             if j == 1:
                 rect = pygame.draw.rect(screen, "white", pygame.Rect(x,y,L,L),1)
                 Rects.append(rect)
@@ -40,25 +39,42 @@ def drawMap():
     del y
 
 while running:
+    collides = []
+    
     screen.fill("black")
+    drawMap()
+    player = pygame.draw.circle(screen, "green", (player_x,player_y),6)
+    
+    collide_index = player.collidelistall(Rects)
+    collide_ls = (itemgetter(*collide_index)(Rects)) if len(collide_index) > 0 else []  
+
+    if len(collide_index) > 1:
+         collides = list(collide_ls)
+    elif len(collide_index) == 1:
+        collides = [collide_ls]
+    
+    def check_stop(player_cord, cord, var):
+        return (not any(player_cord == comp[cord]+var for comp in collides))
+    
+    stop_up = check_stop(player_y,1,40+4)
+    stop_down = check_stop(player_y,1,0-4)
+    stop_left = check_stop(player_x,0,40+4)
+    stop_right = check_stop(player_x,0,0-4)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
         keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_DOWN]:
+
+        if keys[pygame.K_DOWN] and stop_down:
             player_y += STEPS
-        elif keys[pygame.K_UP]:
+        if keys[pygame.K_UP] and stop_up:
             player_y -= STEPS
-        elif keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_RIGHT] and stop_right:
             player_x += STEPS
-        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_LEFT] and stop_left:
             player_x -= STEPS
 
-    drawMap()
-    player = pygame.draw.circle(screen, "green", (player_x,player_y),10)
-    #player.collidelistall(Rects)
-    
     pygame.display.flip()
 
 pygame.quit()
