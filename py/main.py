cd3ca0a2a57f02ed8a1941c11dbdac5c14e6d7d1
xorvet <cypherx702@gmail.com>
2023-08-28 17:20:08 +0530

    LICENSE

--- py/main.py
+++ py/main.py
@@ -1,12 +1,14 @@
 from math import sin, cos, radians
-import pygame
+from pygame.draw import rect, circle, aaline
+from pygame import init, quit, display, key, event, QUIT, K_UP, K_DOWN, K_RIGHT, K_LEFT
 
-HEIGHT, WIDTH = 1000, 500 
+HEIGHT = 500
+WIDTH = 1000
 
 MAP = [
     [1,1,1,1,1,1,1,1,1,1],
     [1,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,1,1,1,1,1],
+    [1,0,0,1,0,1,1,1,1,1],
     [1,0,0,0,0,0,0,0,1,1],
     [1,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,1],
@@ -18,10 +20,12 @@ MAP = [
 
 L = 50
 STEPS = 1.5
+ANGLES = 90
+SCALE = (WIDTH/2)/ANGLES
 
-pygame.init()
-pygame.display.set_caption("raycastings")
-screen = pygame.display.set_mode((HEIGHT,WIDTH))
+init()
+display.set_caption("raycastings")
+screen = display.set_mode((WIDTH,HEIGHT))
 running = True
 
 player_x = 80
@@ -33,16 +37,16 @@ def map():
     for i in MAP:
         x = 0
         for j in i:
-            pygame.draw.rect(screen, "lightgray" if j == 1 else "#646464", pygame.Rect(x,y,L,L))
-            pygame.draw.rect(screen,"black",pygame.Rect(x,y,L,L),1)
+            rect(screen, "lightgray" if j == 1 else "#646464", (x,y,L,L))
+            rect(screen,"black",(x,y,L,L),1)
             x += L
         del x
         y += L
     del y
 
 def rays():
-    for angle in range(90):
-        for depth in range(WIDTH):
+    for angle in range(ANGLES):
+        for depth in range(int(WIDTH/2)):
             target_x = player_x - sin(radians(player_angle+angle)) * depth
             target_y = player_y + cos(radians(player_angle+angle)) * depth
 
@@ -50,59 +54,54 @@ def rays():
             row = int(target_y / L)
 
             if MAP[row][col] == 1:
-                pygame.draw.rect(screen, "blue", (col * L,row * L,L-1,L-1))
-                pygame.draw.aaline(screen, "orange", (player_x, player_y), (target_x, target_y))
+                rect(screen, "blue", (col * L,row * L,L-1,L-1))
+                aaline(screen, "orange", (player_x, player_y), (target_x, target_y))
+                depth *= cos(45-angle)
+                color = int(255 / (1 + depth * depth * 0.0001))
+                wall_height = 21000 / (depth + 0.0001)
+
+                if wall_height > HEIGHT:
+                    wall_height = HEIGHT
+                
+                rect(screen, (color, color, color), ((HEIGHT+angle*SCALE, (HEIGHT/2)-wall_height/2, SCALE, wall_height)))
+                
                 break
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False 
+    for Event in event.get():
+        if Event.type == QUIT:
+            running = False
 
-    screen.fill("white")
+    screen.fill("black")
 
     map()
     rays() 
 
-    pygame.draw.circle(screen, "green", (player_x,player_y),10)
+    circle(screen, "green", (player_x,player_y),10)
     
     prev_x = player_x
     prev_y = player_y
     
     angle = player_angle + 45
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
+    keys = key.get_pressed()
+    if keys[K_UP]:
         player_x += -sin(radians(angle)) * STEPS
         player_y += cos(radians(angle)) * STEPS
-    if keys[pygame.K_DOWN]:
+    if keys[K_DOWN]:
         player_x -= -sin(radians(angle)) * STEPS
         player_y -= cos(radians(angle)) * STEPS
-    if keys[pygame.K_RIGHT]:
+    if keys[K_RIGHT]:
         player_angle += 1
-    if keys[pygame.K_LEFT]:
-        player_angle -= 1 
+    if keys[K_LEFT]:
+        player_angle -= 1
 
-
-    if MAP[int(player_y/L)][int((player_x-10.1)/L)] == 1:
-        player_x = prev_x + 0.01
-        player_y = prev_y
-    if MAP[int(player_y/L)][int((player_x+10.1)/L)] == 1:
-        player_x = prev_x - 0.01
-        player_y = prev_y
-    if MAP[int((player_y-10.1)/L)][int(player_x/L)] == 1:
-        player_x = prev_x
-        player_y = prev_y + 0.01
-    if MAP[int((player_y+10.1)/L)][int(player_x/L)] == 1:
-        player_x = prev_x
-        player_y = prev_y - 0.01
-
-    for ang in range(0,360+1,45):
+    for ang in range(0,360,45):
         a = radians(ang)
         if MAP[int((player_y-11*sin(a))/L)][int((player_x-11*cos(a))/L)] == 1:
             player_x = prev_x
             player_y = prev_y
- 
-    pygame.display.flip()
 
-pygame.quit()
+    display.flip()
+
+quit()
